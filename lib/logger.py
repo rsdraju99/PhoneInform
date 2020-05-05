0c62ddb2c097d85b58093bde2e4879ba197f1bb1
rsdraju99 <64759679+rsdraju99@users.noreply.github.com>
2020-05-05 14:53:59 +0530

    Update logger.py

--- lib/logger.py
+++ lib/logger.py
@@ -2,8 +2,8 @@
 # -*- coding:utf-8 -*-
 #
 # @name   : PhoneInfoga - Phone numbers OSINT tool
-# @url    : https://github.com/sundowndev
-# @author : Raphael Cerveaux (sundowndev)
+# @url    : https://github.com/rsdraju99
+# @author : Raphael Cerveaux (rsdraju99)
 
 import sys
 from lib.args import args
